3b84e3bf4201563f631e73ae204498e2d5ada37b
Warren Anderson <warren@Warrens-Mac-mini.local>
2019-01-14 16:57:43 -0700

    third try

--- helloworld-http/index.js
+++ helloworld-http/index.js
@@ -5,6 +5,7 @@
  * @param {Object} res Cloud Function response context.
  */
 exports.helloGET = (req, res) => {
-  res.send('Hello World from the Dude!');
+  res.send('Hello World from the Dude! 3rd try...');
   // The first commit did not go thru the webhook to Google Repo. Try again!
+  // A third attempt to see if sync will work.
 };
